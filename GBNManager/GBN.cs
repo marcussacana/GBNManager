93e2feb7c3f915a41691a72ca00298cc9767d6d9
Marcus André <ddtank.marcus@gmail.com>
2021-04-23 23:16:12 -0300

    Initial commit

---
+++ GBNManager/GBN.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace GBNManager
+{
+    public class GBN
+    {
+        IGBN Manager;
+
+        public GBN(byte[] Script) : this(Script, Encoding.UTF8) { }
+        public GBN(byte[] Script, Encoding Encoding) {
+            if (Script.Length < 3)
+                throw new Exception("Invalid GBN Script");
+
+            bool HeaderInBegin = Script[0] == 0x47 && Script[0] == 0x42 && Script[0] == 0x4E;
+
+            Manager = HeaderInBegin ? (IGBN)new GSTR(Script, Encoding) : new GBIN(Script, Encoding);
+        }
+
+        public string[] Import() => Manager.Import();
+        public byte[] Export(string[] Content) => Manager.Export(Content);
+
+    }
+
+    interface IGBN {
+        string[] Import();
+        byte[] Export(string[] Content);
+    }
+}
---
+++ GMGUI/Main.cs
@@ -0,0 +1,64 @@
+﻿using GBNManager;
+using System;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace GMGUI
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        GBN Script;
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            Script = new GBN(File.ReadAllBytes(openFileDialog1.FileName));
+            var Text = Script.Import();
+
+            listBox1.Items.Clear();
+            listBox1.Items.AddRange(Text);
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            var Strs = listBox1.Items.Cast<string>().ToArray();
+
+            var NewScript = Script.Export(Strs);
+            File.WriteAllBytes(saveFileDialog1.FileName, NewScript);
+
+            MessageBox.Show("Saved");
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox1.Text = listBox1.Items[listBox1.SelectedIndex].ToString();
+            }
+            catch { }
+        }
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\r' || e.KeyChar == '\n')
+            {
+                listBox1.Items[listBox1.SelectedIndex] = textBox1.Text.Replace("\\n", "\n");
+            }
+        }
+    }
+}
