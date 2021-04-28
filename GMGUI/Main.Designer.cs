d0330a5052ca5e033c18febd5acc95a0dcdc53e8
Marcus André <ddtank.marcus@gmail.com>
2021-04-28 19:34:25 -0300

    Add TextData Support

--- GMGUI/Main.Designer.cs
+++ GMGUI/Main.Designer.cs
@@ -37,6 +37,9 @@ namespace GMGUI
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textdataToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,7 +51,7 @@ namespace GMGUI
             this.listBox1.FormattingEnabled = true;
             this.listBox1.Location = new System.Drawing.Point(12, 12);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(381, 329);
+            this.listBox1.Size = new System.Drawing.Size(381, 342);
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
@@ -66,21 +69,22 @@ namespace GMGUI
             // 
             this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.openToolStripMenuItem,
-            this.saveToolStripMenuItem});
+            this.saveToolStripMenuItem,
+            this.textdataToolStripMenuItem});
             this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 48);
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 92);
             // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.saveToolStripMenuItem.Text = "Save";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
             // 
@@ -93,6 +97,29 @@ namespace GMGUI
             // 
             this.saveFileDialog1.Filter = "All GBIN/GSTR Files|*.gbin;*.gstr";
             this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // textdataToolStripMenuItem
+            // 
+            this.textdataToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem1,
+            this.saveToolStripMenuItem1});
+            this.textdataToolStripMenuItem.Name = "textdataToolStripMenuItem";
+            this.textdataToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.textdataToolStripMenuItem.Text = "Textdata";
+            // 
+            // openToolStripMenuItem1
+            // 
+            this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
+            this.openToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem1.Text = "Open";
+            this.openToolStripMenuItem1.Click += new System.EventHandler(this.openToolStripMenuItem1_Click);
+            // 
+            // saveToolStripMenuItem1
+            // 
+            this.saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
+            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem1.Text = "Save";
+            this.saveToolStripMenuItem1.Click += new System.EventHandler(this.saveToolStripMenuItem1_Click);
             // 
             // Main
             // 
@@ -119,6 +146,9 @@ namespace GMGUI
         private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem textdataToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem1;
     }
 }
 
--- GMGUI/Main.cs
+++ GMGUI/Main.cs
@@ -17,6 +17,9 @@ namespace GMGUI
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
+            TDataMode = false;
+            openFileDialog1.Filter = "All GBIN/GSTR Files|*.gbin;*.gstr";
+            saveFileDialog1.Filter = "All GBIN/GSTR Files|*.gbin;*.gstr";
             openFileDialog1.ShowDialog();
         }
 
@@ -25,22 +28,56 @@ namespace GMGUI
             saveFileDialog1.ShowDialog();
         }
 
+        private void saveToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        bool TDataMode = false;
+        TextData TData;
         GBN Script;
         private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
-            Script = new GBN(File.ReadAllBytes(openFileDialog1.FileName));
-            var Text = Script.Import();
+            if (TDataMode)
+            {
+                TData = new TextData(File.ReadAllBytes(openFileDialog1.FileName)); ;
+                var Text = TData.Import();
 
-            listBox1.Items.Clear();
-            listBox1.Items.AddRange(Text);
+                listBox1.Items.Clear();
+                listBox1.Items.AddRange(Text);
+            }
+            else
+            {
+                Script = new GBN(File.ReadAllBytes(openFileDialog1.FileName));
+                var Text = Script.Import();
+
+                listBox1.Items.Clear();
+                listBox1.Items.AddRange(Text);
+            }
+        }
+
+        private void openToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            TDataMode = true;
+            openFileDialog1.Filter = "All BIN Files|*.bin";
+            saveFileDialog1.Filter = "All BIN Files|*.bin";
+            openFileDialog1.ShowDialog();
         }
 
         private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
         {
             var Strs = listBox1.Items.Cast<string>().ToArray();
+            if (TDataMode)
+            {
+                var NewScript = TData.Export(Strs);
+                File.WriteAllBytes(saveFileDialog1.FileName, NewScript);
+            }
+            else
+            {
 
-            var NewScript = Script.Export(Strs);
-            File.WriteAllBytes(saveFileDialog1.FileName, NewScript);
+                var NewScript = Script.Export(Strs);
+                File.WriteAllBytes(saveFileDialog1.FileName, NewScript);
+            }
 
             MessageBox.Show("Saved");
         }
